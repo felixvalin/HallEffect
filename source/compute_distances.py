67c94e637e8aa862e79081bd42bdf95778d81ab4
felixvalin <felix.valin@mail.mcgill.ca>
2017-11-01 14:13:46 -0400

    new update

--- source/compute_distances.py
+++ source/compute_distances.py
@@ -27,5 +27,12 @@ def compute_distance(caliper1, caliper2, farpoint1, farpoint2, closepoint1, clos
 v5_distance = compute_distance(325.795, 343.303, 182.660, 539.977, 220.267, 495.476)
 v6_distance = compute_distance(391.725, 407.272, 244.076, 557.528, 292.244, 536.679)
 
+l_sample = [9.48, 0.02]
+d_sample = [1.01, 0.02]
+w_sample = [2.70, 0.02]
+
 np.save("../database/distance_v5", v5_distance)
 np.save("../database/distance_v6", v6_distance)
+np.save("../database/l_sample", l_sample)
+np.save("../database/d_sample", d_sample)
+np.save("../database/w_sample", w_sample)
